eb5bcbc8321910b5cdec90fe8b21fb6961fd1ffc
André Büsgen <andre.buesgen@posteo.de>
2025-05-29 13:16:06 +0200

    feat: show current version in header dynamically

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,6 +5,7 @@ import { Button } from "primeng/button";
 import { Tooltip } from "primeng/tooltip";
 import { SettingsComponent } from "@components/settings/settings.component";
 import { Dialog } from "primeng/dialog";
+import packageJson from "../../../../package.json";
 
 @Component({
   selector: "app-header",
@@ -15,6 +16,7 @@ import { Dialog } from "primeng/dialog";
 export class HeaderComponent {
   protected tooltipOptions: TooltipOptions = DEFAULT_TOOLTIP_OPTIONS;
   protected settingsDialogVisible = false;
+  protected version: string = packageJson.version;
 
   protected showDialog(): void {
     this.settingsDialogVisible = true;
