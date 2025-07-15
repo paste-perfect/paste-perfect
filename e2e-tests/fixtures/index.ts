6fde01e7963a8e81c39a28a177d238dcd6860d27
Niklas Büchel <niklas.buechel@accso.de>
2025-07-15 19:45:39 +0200

    fix: test

--- e2e-tests/fixtures/index.ts
+++ e2e-tests/fixtures/index.ts
@@ -6,37 +6,12 @@ export type Page = BasePage & {
   goToPath: (path: string) => Promise<void>;
 };
 
-const STYLESHEET_PATH = './e2e-tests/styles/styles.css'
-
-async function loadTestStyles(page: BasePage): Promise<void> {
-  // Add styles for testing to have a comparable font
-  await page.addStyleTag({
-    path: STYLESHEET_PATH
-  });
-
-  // Wait for fonts to load
-  await page.evaluate(() => {
-    return document.fonts.ready;
-  });
-
-  console.log("Fonts: ", document.fonts);
-  console.log("Stylesheets: ", document.styleSheets)
-
-  // Verify font is loaded
-  const fontLoaded = await page.evaluate(() => {
-    return document.fonts.check('16px Roboto');
-  });
-
-  if (!fontLoaded) {
-    console.warn('Roboto font failed to load, falling back to system fonts');
-  }
-}
+export const STYLESHEET_PATH = './e2e-tests/styles/styles.css'
 
 export const test = baseTest.extend<{
   page: Page;
 }>({
   page: async ({ page }, use) => {
-    await loadTestStyles(page);
 
     page.expectScreenshot = async (name: string, options) => {
       options = {
--- e2e-tests/pages/code-highlighter.page.ts
+++ e2e-tests/pages/code-highlighter.page.ts
@@ -1,8 +1,32 @@
-import { test as base } from "../fixtures";
+import { STYLESHEET_PATH, test as base } from "../fixtures";
 import { createActions } from "../utils/code-highlighter-actions";
 import { createAssertions } from "../utils/code-highlighter-assertions";
 import { createUtils } from "../utils/code-highlighter-utils";
 import { CodeHighlighterPage } from "../types/types";
+import { Page as BasePage } from "playwright-core";
+
+async function loadTestStyles(page: BasePage): Promise<void> {
+  // Add styles for testing to have a comparable font
+  await page.addStyleTag({
+    path: STYLESHEET_PATH,
+  });
+
+  // Wait for fonts to load
+  await page.waitForFunction(() => document.fonts.ready);
+  await page.waitForLoadState("networkidle");
+
+  console.log("Fonts: ", document.fonts);
+  console.log("Stylesheets: ", document.styleSheets);
+
+  // Verify font is loaded
+  const fontLoaded = await page.evaluate(() => {
+    return document.fonts.check("16px Roboto");
+  });
+
+  if (!fontLoaded) {
+    console.warn("Roboto font failed to load, falling back to system fonts");
+  }
+}
 
 /**
  * This fixture 'test' extends the base fixture so we can
@@ -19,8 +43,7 @@ export const test = base.extend<{ page: CodeHighlighterPage }>({
     await page.goToPath("/");
 
     // Wait for fonts to load
-    await page.waitForFunction(() => document.fonts.ready);
-    await page.waitForLoadState("networkidle");
+    await loadTestStyles(page);
 
     // Expose the final typed Page to the tests
     await use(page as CodeHighlighterPage);
