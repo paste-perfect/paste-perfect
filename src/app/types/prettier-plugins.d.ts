79e4e063fb11601096928e8d94c7a252f36f0b33
Niklas B <niklas.buechel@accso.de>
2025-09-28 09:41:58 +0200

    Angular v20 upgrade (#556)
    * refactor(deps): update dependency @primeng/themes to v20
    
    * refactor(deps): update dependency jest-preset-angular to v15
    
    * refactor(deps): update dependency primeng to v20
    
    * refactor(deps): update jest monorepo to v30
    
    * fix: re-generated package-lock.json
    
    * dep: update angular to v20
    
    * fix: added jest-environment-jsdom after jest30 upgrade
    
    * fix: properly migrate code
    
    * fix: removed outdated plugin prettier php
    
    * fix: re-generated package-lock.json
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/app/types/prettier-plugins.d.ts
+++ src/app/types/prettier-plugins.d.ts
@@ -4,12 +4,6 @@ declare module "prettier-plugin-gherkin" {
   export default plugin;
 }
 
-declare module "@prettier/plugin-php" {
-  import { PrettierPlugin } from "@prettier/plugin-php";
-  const plugin: PrettierPlugin;
-  export default plugin;
-}
-
 declare module "prettier-plugin-nginx" {
   import { Plugin } from "prettier-plugin-nginx";
   const plugin: Plugin;
